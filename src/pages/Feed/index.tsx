f104f6721215e9078b7bb897e64f6c5dd333518c
darshakparmar01 <darshak.p@auzmor.com>
2023-04-13 20:54:52 +0530

    AO-editor added quill editor with mention functionality

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RichTextEditor from 'components/RichTextEditor';
 interface IFeedProps {}
 
+interface EditorContentChanged {
+  html: string;
+  markdown: string;
+  json: string;
+}
+
 const Feed: React.FC<IFeedProps> = () => {
-  return <div>Feed Page</div>;
+  const [editorHtmlValue, setEditorHtmlValue] = useState<string>('');
+  const [editorMarkdownValue, setEditorMarkdownValue] = useState<string>('');
+  const [editorJsonValue, setEditorJsonValue] = useState<string>('');
+
+  const onEditorContentChanged = (content: EditorContentChanged) => {
+    setEditorHtmlValue(content.html);
+    setEditorMarkdownValue(content.markdown);
+    setEditorJsonValue(content.json);
+  };
+
+  return <RichTextEditor value={''} onChange={onEditorContentChanged} />;
 };
 
 export default Feed;
