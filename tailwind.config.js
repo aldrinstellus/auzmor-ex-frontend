8c622912b281d20d1d4c664e734164548d969006
Dhruvin Modi <dhruvin.m@american-technology.net>
2023-04-18 14:55:29 +0530

    Init commit for signin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@auzmorui/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -283,5 +286,4 @@ module.exports = {
     },
     plugins: [],
   },
-  plugins: [],
 };
