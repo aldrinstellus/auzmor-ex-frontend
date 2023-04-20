c96c5231c6f40efd4d714ff097df87e3a73a9004
Lovish Sethi <lovish.sethi@american-technology.net>
2023-04-20 12:46:27 +0530

    add disable functionality to button

--- src/components/Comments/CommentForm.tsx
+++ src/components/Comments/CommentForm.tsx
@@ -22,41 +22,39 @@ export const CommentForm: React.FC<CommentFormProps> = ({
     setReplyInputBox(false);
   };
   return (
-    <form onSubmit={onSubmit}>
-      <div className={`flex flex-row items-center ${className} `}>
-        <input
-          className="py-3 px-5 gap-2 box-border border rounded-[32px] border-neutral-200 w-[550px] align-top"
-          value={text}
-          type="text"
-          onChange={(e) => setText(e.target.value)}
-        />
-        <div className="flex ml-[-130px] flex-row items-center">
-          <button>
-            <img
-              className="mx-2 cursor-pointer"
-              width={24}
-              height={24}
-              src={Image}
-            />
-          </button>
-          <button>
-            <img
-              className="mx-2 cursor-pointer"
-              width={24}
-              height={24}
-              src={Smily}
-            />
-          </button>
-          <button onClick={onSubmit}>
-            <img
-              className="mx-2 cursor-pointer"
-              width={24}
-              height={24}
-              src={Send}
-            />
-          </button>
-        </div>
+    <div className={`flex flex-row items-center ${className} `}>
+      <input
+        className="py-3 px-5 gap-2 box-border border rounded-[32px] border-neutral-200 w-[550px] align-top"
+        value={text}
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <div className="flex ml-[-130px] flex-row items-center">
+        <button>
+          <img
+            className="mx-2 cursor-pointer"
+            width={24}
+            height={24}
+            src={Image}
+          />
+        </button>
+        <button>
+          <img
+            className="mx-2 cursor-pointer"
+            width={24}
+            height={24}
+            src={Smily}
+          />
+        </button>
+        <button disabled={text ? false : true} onClick={onSubmit}>
+          <img
+            className="mx-2 cursor-pointer"
+            width={24}
+            height={24}
+            src={Send}
+          />
+        </button>
       </div>
-    </form>
+    </div>
   );
 };
