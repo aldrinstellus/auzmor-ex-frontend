7e15d3ca5af7881bceeb251f4afdff226661e397
Akshay Rajpurohit <akshay.r@auzmor.com>
2023-05-04 23:44:22 +0530

    Change card top position

--- src/components/RenderQuillDelta/components/Mention/index.tsx
+++ src/components/RenderQuillDelta/components/Mention/index.tsx
@@ -14,7 +14,7 @@ export const Mention: React.FC<MentionProps> = (
         <MentionUserCard
           fullName="Akshay Rajpurohit"
           email="akshay.r@auzmor.com"
-          className="absolute -top-36 z-10 left-0 shadow-lg opacity-0 transition-opacity duration-200 hover:opacity-100 min-w-max"
+          className="absolute top-0 z-10 left-0 shadow-lg opacity-0 transition-opacity duration-200 hover:opacity-100 min-w-max"
         />
         <span className="cursor-pointer" contentEditable="false">
           <span className="ql-mention-denotation-char">{props?.value}</span>
