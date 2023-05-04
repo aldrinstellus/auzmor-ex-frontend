0d7e34063faa42ffad5ad5b8a11ee4d080a4533a
Rajesh Soni <rajesh.soni@american-technology.net>
2023-05-04 12:14:03 +0530

    Appshell screen size

--- src/components/AppShell/index.tsx
+++ src/components/AppShell/index.tsx
@@ -1,24 +1,18 @@
 import * as React from 'react';
 import Navbar from './components/Navbar';
-import { useLocation } from 'react-router-dom';
 
 export interface IAppShellProps {
   children: React.ReactNode;
 }
 
 const AppShell: React.FC<IAppShellProps> = ({ children }) => {
-  const location = useLocation();
   return (
-    <div className="w-screen bg-neutral-100">
+    <div className="bg-neutral-100">
       <Navbar />
-      {location.pathname === '/feed' ? (
-        children
-      ) : (
-        <div className="pt-8 px-14 flex w-full h-full justify-center">
-          {children}
-          {/* <div className="max-w-[1600px]">{children}</div> */}
-        </div>
-      )}
+      <div className="pt-8 px-14 flex w-full h-full justify-center">
+        {children}
+        {/* <div className="max-w-[1600px]">{children}</div> */}
+      </div>
     </div>
   );
 };
