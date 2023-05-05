0789298ca926e1a4dff4e92f00a5bea9295f0cb7
Rajesh Soni <rajesh.soni@american-technology.net>
2023-05-05 16:22:45 +0530

    Fallback to non virtual list

--- src/queries/post.ts
+++ src/queries/post.ts
@@ -37,11 +37,6 @@ export const createPost = async (payload: IPost) => {
   return data;
 };
 
-export const fetchFeed = ({ pageParam = null }) => {
-  if (pageParam === null) return apiService.get('/posts');
-  else return apiService.get(pageParam);
-};
-
 export const getPreviewLink = async (previewUrl: string) => {
   const { data } = await apiService.get(`/links/unfurl?url=${previewUrl}`);
   return data;
@@ -65,14 +60,21 @@ export const deletePost = async (id: string) => {
   return data;
 };
 
+export const fetchFeed = ({ pageParam = null }) => {
+  if (pageParam === null) return apiService.get('/posts');
+  else return apiService.get(pageParam);
+};
+
 export const useInfiniteFeed = (q?: Record<string, any>) => {
-  return useInfiniteQuery(['feed', q], fetchFeed, {
+  return useInfiniteQuery({
+    queryKey: ['feed', q],
+    queryFn: fetchFeed,
     getNextPageParam: (lastPage: any) => {
       return lastPage?.data?.result?.paging?.next;
     },
     getPreviousPageParam: (currentPage: any) => {
       return currentPage?.data?.result?.paging?.prev;
     },
-    onSuccess: (data) => console.log(data),
+    staleTime: 5 * 60 * 1000,
   });
 };
