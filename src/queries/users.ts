8a779052e8e2850f43186b9ef4e6365ad7196a91
Dhruvin Modi <dhruvin.m@american-technology.net>
2023-05-11 23:48:21 +0530

    Invite members

--- src/queries/users.ts
+++ src/queries/users.ts
@@ -1,6 +1,44 @@
 import { useQuery } from '@tanstack/react-query';
 import apiService from 'utils/apiService';
 
+export interface IPostUser {
+  fullName: string;
+  workEmail: string;
+  role: string;
+}
+
+export interface IPostUsers {
+  users: IPostUser[];
+}
+
+export enum UserStatus {
+  Created = 'CREATED',
+  Invited = 'INVITED',
+  Attempted = 'ATTEMPTED',
+  Active = 'ACTIVE',
+  Inactive = 'INACTIVE',
+  Deleted = 'DELETED',
+  Failed = 'FAILED',
+}
+
+export enum UserRole {
+  Member = 'MEMBER',
+  Admin = 'ADMIN',
+  Superadmin = 'SUPERADMIN',
+}
+
+export interface IPostUsersResponse {
+  id?: string;
+  createdAt: string | null;
+  fullName: string;
+  message: string;
+  organization: string;
+  reason: string;
+  role: UserRole;
+  status: UserStatus;
+  workEmail: string;
+}
+
 const getAllUsers = async (q: Record<string, any>) => {
   const { data } = await apiService.get('/users', q);
   return data;
@@ -14,8 +52,8 @@ export const useUsers = (q: Record<string, any>) => {
   });
 };
 
-export const inviteUsers = async (q: Record<string, any>) => {
-  const data = await apiService.post('/users', q);
+export const inviteUsers = async (payload: IPostUsers) => {
+  const data = await apiService.post('/users', payload);
   return new Promise((res) => {
     res(data);
   });
