f334360f76eb98af5e2ce6c6415a3cc7040319bf
Akshay Rajpurohit <akshay.r@auzmor.com>
2023-06-27 12:10:19 +0530

    AO-620 | Update logo and styling

--- src/pages/Unsupported/index.tsx
+++ src/pages/Unsupported/index.tsx
@@ -5,9 +5,9 @@ import GreenLogo from 'images/greenLogo.png';
 const Unsupported: React.FC = (): ReactElement => {
   return (
     <div className="flex items-center justify-center w-screen h-screen p-6">
-      <div className="flex flex-col justify-between items-center">
-        <img src={GreenLogo} width={300} height={300} />
-        <img src={NotSupported} width={600} height={600} />
+      <div className="flex flex-col justify-between items-center gap-y-36">
+        <img src={GreenLogo} width={130} height={50} />
+        <img src={NotSupported} width={300} height={300} />
         <p className="text-base font-semibold">
           Sorry we do not support mobile devices yet!
         </p>
