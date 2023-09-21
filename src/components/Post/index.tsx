ae15ae9de91005f227df460ac489bf840d809112
Dinesh Shaw <idineshshaw7@gmail.com>
2023-09-21 12:16:19 +0530

    use visibility sensor to virtualise feed

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router';
 import moment from 'moment';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import VisibilitySensor from 'react-visibility-sensor';
 
 // components
 import Card from 'components/Card';
@@ -71,16 +72,22 @@ const Post: FC<PostProps> = ({ post, customNode = null }) => {
   const [showComments, openComments, closeComments] = useModal(false);
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
   const [showReactionModal, openReactionModal, closeReactionModal] =
     useModal(false);
+
   const reaction = post?.myReaction?.reaction;
+
   const totalCount = Object.values(post.reactionsCount || {}).reduce(
     (total, count) => total + count,
     0,
   );
   const getPost = useFeedStore((state) => state.getPost);
   const updateFeed = useFeedStore((state) => state.updateFeed);
+
+  const cardRef = useRef<HTMLInputElement>(null);
   const previousShowComment = useRef<boolean>(false);
+
   const { currentTimezone } = useCurrentTimezone();
 
   const createBookmarkMutation = useMutation({
@@ -175,179 +182,196 @@ const Post: FC<PostProps> = ({ post, customNode = null }) => {
 
   return (
     <>
-      <Card className="flex flex-col">
-        <AcknowledgementBanner data={post} />
-        <div className="post-content p-6 flex flex-col gap-4">
-          <div className="flex gap-4 justify-between items-start p-1">
-            <Actor
-              contentMode={VIEW_POST}
-              createdTime={humanizeTime(post.createdAt!)}
-              createdBy={post?.createdBy}
-              audience={post.audience}
-              dataTestId="feedpage-activity-username"
-              entityId={post.id}
-              postType={post.type}
-            />
-
-            <Tooltip
-              tooltipContent={
-                post.bookmarked ? 'Remove from bookmark' : 'Bookmark post'
-              }
-              tooltipPosition="top"
-            >
-              <Icon
-                name={post.bookmarked ? 'postBookmarkFilled' : 'postBookmark'}
-                size={24}
-                dataTestId="feed-post-bookmark"
-                onClick={() => handleBookmarkClick(post)}
-                isActive={post.bookmarked}
-              />
-            </Tooltip>
-            <div className="relative">
-              <FeedPostMenu data={post as unknown as IPost} />
-            </div>
-          </div>
-
-          {post?.schedule && (
-            <div className="flex items-center gap-2 bg-primary-50 justify-between px-3 py-2">
-              <Icon name="calendarOutline" size={16} color="text-neutral-900" />
-              <div className="text-xs font-medium text-neutral-600 flex-1">
-                Post scheduled for{' '}
-                {getTimeInScheduleFormat(
-                  new Date(post?.schedule.dateTime),
-                  moment(post?.schedule.dateTime).format('h:mm a'),
-                  post?.schedule.timeZone,
-                  currentTimezone,
-                )}
-              </div>
-              <div className="flex items-center gap-4">
-                <Icon
-                  name="editOutline"
-                  size={16}
-                  color="text-neutral-900"
-                  onClick={openEditSchedulePostModal}
-                />
-                <div
-                  className="text-xs font-bold whitespace-nowrap text-neutral-900 
-                underline cursor-pointer hover:text-primary-500 decoration-neutral-400 hover:decoration-primary-400"
-                  onClick={openPublishModal}
-                  data-testid="scheduledpost-tab-publishnow"
-                >
-                  Publish now
-                </div>
-              </div>
-            </div>
-          )}
-
-          <RenderQuillContent data={post} />
-          {/* Reaction Count */}
-
-          {(totalCount > 0 || post?.commentsCount > 0) && !!!post.schedule && (
-            <div className="flex flex-row justify-between py-3 border-y-1 border-y-neutral-100">
-              <div
-                className={`flex flex-row items-center space-x-1 group`}
-                data-testid="feed-post-reactioncount"
-                onClick={() => openReactionModal()}
-              >
-                {totalCount > 0 && (
-                  <div className="flex">
-                    {Object.keys(post.reactionsCount)
-                      .filter(
-                        (key) =>
-                          !!post.reactionsCount[key] &&
-                          post.reactionsCount[key] > 0,
-                      )
-                      .slice(0, 3)
-                      .map((key, i) => (
-                        <div
-                          className={`${
-                            i > 0 ? 'p-[1px] -ml-[8px] z-1' : 'p-[1px]'
-                          }`}
-                          key={key}
-                        >
-                          <Icon name={`${key}Reaction`} size={20} />
-                        </div>
-                      ))}
-                  </div>
-                )}
-                {totalCount > 0 && (
-                  <div
-                    className={`flex text-xs font-normal text-neutral-500 cursor-pointer group-hover:text-primary-500`}
-                  >
-                    {totalCount} reacted
-                  </div>
-                )}
-              </div>
-              {post?.commentsCount > 0 && (
-                <div className="flex flex-row text-xs font-normal text-neutral-500 space-x-7 items-center cursor-pointer hover:text-primary-500">
-                  <div
-                    onClick={() => {
-                      if (showComments) {
-                        closeComments();
-                      } else {
-                        openComments();
+      <div ref={cardRef}>
+        <VisibilitySensor
+          partialVisibility
+          resizeCheck
+          scrollCheck
+          offset={{ top: -500, bottom: -500 }}
+        >
+          {({ isVisible }: { isVisible: boolean }) =>
+            isVisible ? (
+              <Card className="flex flex-col">
+                <AcknowledgementBanner data={post} />
+                <div className="post-content p-6 flex flex-col gap-4">
+                  <div className="flex gap-4 justify-between items-start p-1">
+                    <Actor
+                      contentMode={VIEW_POST}
+                      createdTime={humanizeTime(post.createdAt!)}
+                      createdBy={post?.createdBy}
+                      audience={post.audience}
+                      dataTestId="feedpage-activity-username"
+                      entityId={post.id}
+                      postType={post.type}
+                    />
+                    <Tooltip
+                      tooltipContent={
+                        post.bookmarked
+                          ? 'Remove from bookmark'
+                          : 'Bookmark post'
                       }
-                    }}
-                    data-testid="feed-post-commentscount"
-                  >
-                    {post.commentsCount || 0}{' '}
-                    {getNouns('comment', post?.commentsCount || 0)}
+                      tooltipPosition="top"
+                    >
+                      <Icon
+                        name={
+                          post.bookmarked
+                            ? 'postBookmarkFilled'
+                            : 'postBookmark'
+                        }
+                        size={24}
+                        dataTestId="feed-post-bookmark"
+                        onClick={() => handleBookmarkClick(post)}
+                        isActive={post.bookmarked}
+                      />
+                    </Tooltip>
+                    <div className="relative">
+                      <FeedPostMenu data={post as unknown as IPost} />
+                    </div>
                   </div>
-                  {/* <div data-testid="feed-post-repostcount">0 reposts</div> */}
+                  {post?.schedule && (
+                    <div className="flex items-center gap-2 bg-primary-50 justify-between px-3 py-2">
+                      <Icon
+                        name="calendarOutline"
+                        size={16}
+                        color="text-neutral-900"
+                      />
+                      <div className="text-xs font-medium text-neutral-600 flex-1">
+                        Post scheduled for{' '}
+                        {getTimeInScheduleFormat(
+                          new Date(post?.schedule.dateTime),
+                          moment(post?.schedule.dateTime).format('h:mm a'),
+                          post?.schedule.timeZone,
+                          currentTimezone,
+                        )}
+                      </div>
+                      <div className="flex items-center gap-4">
+                        <Icon
+                          name="editOutline"
+                          size={16}
+                          color="text-neutral-900"
+                          onClick={openEditSchedulePostModal}
+                        />
+                        <div
+                          className="text-xs font-bold whitespace-nowrap text-neutral-900 underline cursor-pointer hover:text-primary-500 decoration-neutral-400 hover:decoration-primary-400"
+                          onClick={openPublishModal}
+                          data-testid="scheduledpost-tab-publishnow"
+                        >
+                          Publish now
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  <RenderQuillContent data={post} />
+                  {/* Reaction Count */}
+                  {(totalCount > 0 || post?.commentsCount > 0) &&
+                    !!!post.schedule && (
+                      <div className="flex flex-row justify-between py-3 border-y-1 border-y-neutral-100">
+                        <div
+                          className={`flex flex-row items-center space-x-1 group`}
+                          data-testid="feed-post-reactioncount"
+                          onClick={() => openReactionModal()}
+                        >
+                          {totalCount > 0 && (
+                            <div className="flex">
+                              {Object.keys(post.reactionsCount)
+                                .filter(
+                                  (key) =>
+                                    !!post.reactionsCount[key] &&
+                                    post.reactionsCount[key] > 0,
+                                )
+                                .slice(0, 3)
+                                .map((key, i) => (
+                                  <div
+                                    className={`${
+                                      i > 0
+                                        ? 'p-[1px] -ml-[8px] z-1'
+                                        : 'p-[1px]'
+                                    }`}
+                                    key={key}
+                                  >
+                                    <Icon name={`${key}Reaction`} size={20} />
+                                  </div>
+                                ))}
+                            </div>
+                          )}
+                          {totalCount > 0 && (
+                            <div
+                              className={`flex text-xs font-normal text-neutral-500 cursor-pointer group-hover:text-primary-500`}
+                            >
+                              {totalCount} reacted
+                            </div>
+                          )}
+                        </div>
+                        {post?.commentsCount > 0 && (
+                          <div className="flex flex-row text-xs font-normal text-neutral-500 space-x-7 items-center cursor-pointer hover:text-primary-500">
+                            <div
+                              onClick={() => {
+                                if (showComments) {
+                                  closeComments();
+                                } else {
+                                  openComments();
+                                }
+                              }}
+                              data-testid="feed-post-commentscount"
+                            >
+                              {post.commentsCount || 0}{' '}
+                              {getNouns('comment', post?.commentsCount || 0)}
+                            </div>
+                            {/* <div data-testid="feed-post-repostcount">0 reposts</div> */}
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  {!!!post.schedule && (
+                    <div className="flex justify-between">
+                      <div className="flex space-x-6">
+                        {/* this is for post */}
+                        <Likes
+                          reaction={reaction || ''}
+                          entityId={post?.id || ''}
+                          entityType="post"
+                          reactionId={post?.myReaction?.id || ''}
+                          queryKey="feed"
+                          dataTestIdPrefix="post-reaction"
+                        />
+                        <Button
+                          label="Comment"
+                          variant={Variant.Tertiary}
+                          size={Size.Small}
+                          labelClassName="text-xs font-normal text-neutral-500 hover:text-primary-500"
+                          leftIcon="comment"
+                          className="space-x-1 !p-0"
+                          onClick={() => {
+                            if (showComments) {
+                              closeComments();
+                            } else {
+                              openComments();
+                            }
+                          }}
+                          data-testid="feed-post-comment"
+                        />
+                      </div>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          )}
+                {/* Comments */}
+                {showComments ? (
+                  <div className="pb-3 px-3">
+                    <CommentCard entityId={post?.id || ''} />
+                  </div>
+                ) : (
+                  !previousShowComment.current && customNode
+                )}
+              </Card>
+            ) : (
+              <div
+                style={{ minHeight: cardRef?.current?.clientHeight || 400 }}
+              />
+            )
+          }
+        </VisibilitySensor>
+      </div>
 
-          {!!!post.schedule && (
-            <div className="flex justify-between">
-              <div className="flex space-x-6">
-                {/* this is for post */}
-                <Likes
-                  reaction={reaction || ''}
-                  entityId={post?.id || ''}
-                  entityType="post"
-                  reactionId={post?.myReaction?.id || ''}
-                  queryKey="feed"
-                  dataTestIdPrefix="post-reaction"
-                />
-                <Button
-                  label="Comment"
-                  variant={Variant.Tertiary}
-                  size={Size.Small}
-                  labelClassName="text-xs font-normal text-neutral-500 hover:text-primary-500"
-                  leftIcon="comment"
-                  className="space-x-1 !p-0"
-                  onClick={() => {
-                    if (showComments) {
-                      closeComments();
-                    } else {
-                      openComments();
-                    }
-                  }}
-                  data-testid="feed-post-comment"
-                />
-              </div>
-              {/* <div
-                className="flex items-center space-x-1 cursor-pointer text-neutral-500 hover:text-primary-500"
-                data-testid="feed-post-repost"
-              >
-                <Icon name="repost" size={16} />
-                <span className="text-xs font-normal">Repost</span>
-              </div> */}
-            </div>
-          )}
-        </div>
-
-        {/* Comments */}
-        {showComments ? (
-          <div className="pb-3 px-3">
-            <CommentCard entityId={post?.id || ''} />
-          </div>
-        ) : (
-          !previousShowComment.current && customNode
-        )}
-      </Card>
       {showReactionModal && (
         <ReactionModal
           closeModal={() => closeReactionModal()}
