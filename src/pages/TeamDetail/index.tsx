b02f4e0e85ded6134e26196e10ccb58f976ba977
Seram Nitesh Singh <nitesh.s@auzmor.com>
2023-09-22 19:12:37 +0530

    Redirect to not found page for team and user detail

--- src/pages/TeamDetail/index.tsx
+++ src/pages/TeamDetail/index.tsx
@@ -19,7 +19,12 @@ import queryClient from 'utils/queryClient';
 import SuccessToast from 'components/Toast/variants/SuccessToast';
 import useModal from 'hooks/useModal';
 import People from 'pages/Users/components/People';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from 'react-router-dom';
 import TeamModal from 'pages/Users/components/TeamModal';
 import { TeamFlow, TeamTab } from 'pages/Users/components/Teams';
 import DeleteTeam from 'pages/Users/components/DeleteModals/Team';
@@ -116,6 +121,10 @@ const TeamDetail: FC<ITeamMemberProps> = () => {
       navigate(`/teams`);
     }
   };
+
+  if (!teamDetail?.isLoading && !data) {
+    return <Navigate to="/404" />;
+  }
 
   return (
     <>
