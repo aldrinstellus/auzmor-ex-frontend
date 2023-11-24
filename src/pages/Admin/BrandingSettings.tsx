536e8fcb32274c9e572d46aa5986a83356b8a6f8
DhruvinModi <dhruvin.m@american-technology.net>
2023-11-24 10:59:01 +0530

    [WIP]: login layout

--- src/pages/Admin/BrandingSettings.tsx
+++ src/pages/Admin/BrandingSettings.tsx
@@ -1,4 +1,4 @@
-import Button, { Variant, Size } from 'components/Button';
+import Button, { Variant } from 'components/Button';
 import Card from 'components/Card';
 import Collapse from 'components/Collapse';
 import Divider from 'components/Divider';
@@ -14,18 +14,33 @@ import {
   MediaValidationError,
 } from 'contexts/CreatePostContext';
 import { MB } from 'utils/constants';
+import { IRadioListOption } from 'components/RadioGroup';
 
 const BrandingSettings: FC = () => {
-  const { control, setValue } = useForm();
+  const { control, setValue, watch } = useForm();
   const [selectedLogo, setSelectedLogo] = useState<File | null>(null);
   const [selectedFavicon, setSelectedFavicon] = useState<File | null>(null);
+  const [selectedBG, setSelectedBG] = useState<File | null>(null);
   const [logoValidation, setLogoValidation] = useState<IMediaValidationError[]>(
     [],
   );
   const [faviconValidation, setFaviconValidation] = useState<
     IMediaValidationError[]
   >([]);
+  const [bgValidation, setBGValidation] = useState<IMediaValidationError[]>([]);
   const [showSecondaryColor, setShowSecondaryColor] = useState(false);
+  const [layoutAlignment, setLayoutAlignment] = useState<
+    'CENTER' | 'LEFT' | 'RIGHT'
+  >('RIGHT');
+
+  const [primaryColor, secondaryColor, loginBackgroundType] = watch([
+    'primaryColor',
+    'secondaryColor',
+    'loginBackgroundType',
+  ]);
+
+  console.log(loginBackgroundType);
+
   const { getRootProps: getRootPropsLogo, getInputProps: getInputPropsLogo } =
     useDropzone({
       onDrop: (acceptedFiles) => {
@@ -128,7 +143,7 @@ const BrandingSettings: FC = () => {
     },
     validator: (file) => {
       // size validation
-      if (file.size > 1 * MB) {
+      if (file.size > 5 * MB) {
         setFaviconValidation([
           ...faviconValidation.filter(
             (error) => error.errorType !== MediaValidationError.ImageSizeExceed,
@@ -164,13 +179,75 @@ const BrandingSettings: FC = () => {
     },
   });
 
-  const resetValidationError = (
-    type:
-      | 'LOGO'
-      | 'FAVICON'
-      | 'LOGIN_BACKGROUND_IMAGE'
-      | 'LOGIN_BACKGROUND_VIDEO',
-  ) => {
+  const { getRootProps: getRootPropsBG, getInputProps: getInputPropsBG } =
+    useDropzone({
+      onDrop: (acceptedFiles) => {
+        setSelectedBG(acceptedFiles[0]);
+      },
+      maxFiles: 1,
+      accept: {
+        'image/png': ['.png'],
+        'image/svg': ['.svg'],
+        'image/jpeg': ['.jpg', '.jpeg'],
+      },
+      onDropRejected: (rejection) => {
+        // extension validation
+        const error = rejection[0].errors[0];
+        const fimeName = rejection[0].file.name;
+        if (error.code === 'file-invalid-type') {
+          setBGValidation([
+            ...faviconValidation.filter(
+              (error) =>
+                error.errorType !== MediaValidationError.FileTypeNotSupported,
+            ),
+            {
+              errorMsg: `File type must be .png, .jpg, .jpeg, .svg, .ico`,
+              errorType: MediaValidationError.IncorrectDimension,
+              fileName: fimeName,
+            },
+          ]);
+        }
+      },
+      validator: (file) => {
+        // size validation
+        if (file.size > 1 * MB) {
+          setBGValidation([
+            ...faviconValidation.filter(
+              (error) =>
+                error.errorType !== MediaValidationError.ImageSizeExceed,
+            ),
+            {
+              errorMsg: `Max file size 5mb`,
+              errorType: MediaValidationError.ImageSizeExceed,
+              fileName: file.name,
+            },
+          ]);
+        }
+
+        // dimension validation
+        const image = new Image();
+        image.src = getMediaObj([file])[0].original;
+        image.onload = () => {
+          const { height, width } = image;
+          if (height !== 32 || width !== 32) {
+            setBGValidation([
+              ...logoValidation.filter(
+                (error) =>
+                  error.errorType !== MediaValidationError.IncorrectDimension,
+              ),
+              {
+                errorMsg: `Dimension should be 32 x 32 px`,
+                errorType: MediaValidationError.IncorrectDimension,
+                fileName: file.name,
+              },
+            ]);
+          }
+        };
+        return null;
+      },
+    });
+
+  const resetValidationError = (type: 'LOGO' | 'FAVICON' | 'BG') => {
     switch (type) {
       case 'LOGO':
         setLogoValidation([]);
@@ -341,7 +418,7 @@ const BrandingSettings: FC = () => {
                     control,
                     className: '',
                     dataTestId: 'primary-color',
-                    defaultValue: '#FFFFFF',
+                    defaultValue: '#10B981',
                     setValue,
                   },
                 ]}
@@ -356,7 +433,7 @@ const BrandingSettings: FC = () => {
                       control,
                       className: '',
                       dataTestId: 'secondary-color',
-                      defaultValue: '#FFFFFF',
+                      defaultValue: '#1d4ed8',
                       setValue,
                     },
                   ]}
@@ -372,7 +449,10 @@ const BrandingSettings: FC = () => {
               )}
             </div>
             <div className="w-[182px] flex flex-col rounded-7xl overflow-hidden gap-1 border border-neutral-200 shadow-sm">
-              <div className="p-3 flex bg-blue-700 gap-2 px-2.5">
+              <div
+                className="p-3 flex gap-2 px-2.5"
+                style={{ backgroundColor: secondaryColor || '#1d4ed8' }}
+              >
                 <Icon
                   name="flashIcon"
                   className="text-white"
@@ -388,7 +468,13 @@ const BrandingSettings: FC = () => {
                 Lorem ipsum dolor si amet. Lorem ipsum dolor si amet
               </p>
               <img src={NoAnnouncement} className="h-[70px]" />
-              <Button label="Primary" className="mx-2.5" size={Size.Small} />
+
+              <div
+                className="flex items-center justify-center text-white rounded-19xl px-2.5 py-2 mx-2.5 text-sm font-bold"
+                style={{ backgroundColor: primaryColor || '#10B981' }}
+              >
+                Primary
+              </div>
               <p className="font-normal text-[8px] text-neutral-500 text-center px-2.5 mb-4">
                 Only admins can see this.
               </p>
@@ -413,22 +499,46 @@ const BrandingSettings: FC = () => {
                   Layout alignment
                 </p>
                 <div className="flex gap-[60px]">
-                  <div className="flex flex-col items-center gap-2">
-                    <div className="w-[100px] h-[60px] bg-neutral-100 relative border border-neutral-200 rounded-7xl">
+                  <div
+                    className="flex flex-col items-center gap-2"
+                    onClick={() => setLayoutAlignment('LEFT')}
+                  >
+                    <div
+                      className={`w-[100px] h-[60px] bg-neutral-100 relative border border-neutral-200 rounded-7xl ${
+                        layoutAlignment === 'LEFT' &&
+                        'border-primary-500 border-2'
+                      }`}
+                    >
                       <div className="absolute top-0 left-0 h-full w-[40px] rounded-7xl bg-neutral-400"></div>
                     </div>
                     <p className="text-neutral-900 font-medium text-sm">Left</p>
                   </div>
-                  <div className="flex flex-col items-center gap-2">
-                    <div className="w-[100px] h-[60px] bg-neutral-100 flex justify-center border border-neutral-200 rounded-7xl">
+                  <div
+                    className="flex flex-col items-center gap-2"
+                    onClick={() => setLayoutAlignment('CENTER')}
+                  >
+                    <div
+                      className={`w-[100px] h-[60px] bg-neutral-100 flex justify-center border border-neutral-200 rounded-7xl ${
+                        layoutAlignment === 'CENTER' &&
+                        'border-primary-500 border-2'
+                      }`}
+                    >
                       <div className="h-full w-[40px] rounded-7xl bg-neutral-400"></div>
                     </div>
                     <p className="text-neutral-900 font-medium text-sm">
                       Center
                     </p>
                   </div>
-                  <div className="flex flex-col items-center gap-2">
-                    <div className="w-[100px] h-[60px] bg-neutral-100 relative border border-neutral-200 rounded-7xl">
+                  <div
+                    className="flex flex-col items-center gap-2"
+                    onClick={() => setLayoutAlignment('RIGHT')}
+                  >
+                    <div
+                      className={`w-[100px] h-[60px] bg-neutral-100 relative border border-neutral-200 rounded-7xl ${
+                        layoutAlignment === 'RIGHT' &&
+                        'border-primary-500 border-2'
+                      }`}
+                    >
                       <div className="absolute top-0 right-0 h-full w-[40px] rounded-7xl bg-neutral-400"></div>
                     </div>
                     <p className="text-neutral-900 font-medium text-sm">
@@ -437,8 +547,88 @@ const BrandingSettings: FC = () => {
                   </div>
                 </div>
               </div>
+              <div className="flex flex-col gap-3">
+                <p className="text-sm font-bold text-neutral-900">
+                  Select Background type
+                </p>
+                <Layout
+                  fields={[
+                    {
+                      type: FieldType.Radio,
+                      name: 'loginBackgroundType',
+                      className: 'flex !flex-row gap-4',
+                      control,
+                      radioList: [
+                        { data: { type: 'Color' }, dataTestId: 'color' },
+                        { data: { type: 'Video' }, dataTestId: 'video' },
+                        { data: { type: 'Image' }, dataTestId: 'image' },
+                      ],
+                      labelRenderer: (option: IRadioListOption) => {
+                        return (
+                          <p className="pl-1 text-sm">{option.data.type}</p>
+                        );
+                      },
+                    },
+                  ]}
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <p className="text-sm font-bold text-neutral-900">
+                  Upload Image
+                </p>
+                <div
+                  {...getRootPropsBG()}
+                  className="border border-dashed border-neutral-200 rounded-9xl p-6 w-full h-[186px] flex justify-center items-center"
+                >
+                  <input {...getInputPropsBG()} />
+                  {selectedBG && !!!bgValidation?.length ? (
+                    <div className="max-h-full max-w-full relative">
+                      <img
+                        src={getMediaObj([selectedBG])[0].original}
+                        className="max-h-full max-w-full"
+                      />
+                      <div
+                        className="absolute -right-3 -top-3 w-6 h-6 rounded-full flex items-center justify-center bg-black group"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setSelectedBG(null);
+                          resetValidationError('BG');
+                        }}
+                      >
+                        <Icon name="close" size={16} color="text-white" />
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex flex-col justify-center items-center gap-2">
+                      <Icon
+                        name="documentUpload"
+                        color="text-neutral-900"
+                        hover={false}
+                      />
+                      <div className="text-neutral-900 font-medium">
+                        Upload Image
+                      </div>
+                      <div className="mt-1 text-neutral-500 text-xs text-center">
+                        Drag and drop or click here to upload file. <br /> Ideal
+                        image size: 1920 x 860 px
+                      </div>
+                    </div>
+                  )}
+                </div>
+                {bgValidation?.length > 0 && (
+                  <p className="text-xxs text-neutral-500">
+                    {bgValidation[0].errorMsg}
+                  </p>
+                )}
+              </div>
             </div>
-            <div className="flex"></div>
+            <div className="flex">
+              <iframe
+                width={400}
+                height={180}
+                src="https://office.auzmor.com"
+              />
+            </div>
           </div>
         </div>
       </Collapse>
