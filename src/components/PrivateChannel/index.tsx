6b359bad0a520f0f2340f43262bec897edc68759
Anshul Verma <anshul.verma@american-technology.net>
2024-02-06 14:38:37 +0530

    add channel request & channel widget in feed , wireframe api .

--- src/components/PrivateChannel/index.tsx
+++ src/components/PrivateChannel/index.tsx
@@ -5,68 +5,62 @@ import PrivateChannelImage from 'images/png/PrivateChannelBanner.png';
 import Button, { Variant } from 'components/Button';
 import AvatarChips from 'components/AvatarChips';
 import { useTranslation } from 'react-i18next';
+import { useChannelAdmins } from 'queries/channel';
+import { useNavigate } from 'react-router-dom';
 
 interface IChannelBannerProps {
-  channelAdmin?: IChannel; //
   isChannelRequest?: boolean;
+  channelId?: string;
+  dataTestId?: string; // Add dataTestId prop for custom test ids
 }
 
 const PrivateChannelBanner: FC<IChannelBannerProps> = ({
+  channelId = '',
   isChannelRequest = true,
+  dataTestId = 'channels-widget', // Initialize dataTestId with empty string as default
 }) => {
   const { t } = useTranslation('channel');
-
-  // api call to get the admin of  particular channel .
-  //  pass a channel id as a props
-  const adminDummyData = [
-    {
-      userId: '6516afa8dc958600e4619cdf',
-      email: 'Sim_Kris@hotmail.com',
-      fullName: 'Abby Wolff',
-      status: 'ACTIVE',
-      designation: 'Quality Assurance',
-      department: 'Grocery',
-      profileImage: {
-        id: '653f87281ef0a90b7d1e7b54',
-        original:
-          'https://office-qa-cdn.auzmor.com/646311ed765368daacc601f7/public/users/646311ed765368daacc601f9/profile/1698662184834-original.jpg',
-        blurHash: '',
-      },
-    },
-    {
-      userId: '6516afa8dc958600e4619cdf',
-      email: 'Sim_Kris@hotmail.com',
-      fullName: 'Abby Wolff',
-      status: 'ACTIVE',
-      designation: 'Quality Assurance',
-      department: 'Grocery',
-      profileImage: {
-        id: '653f87281ef0a90b7d1e7b54',
-        original:
-          'https://office-qa-cdn.auzmor.com/646311ed765368daacc601f7/public/users/646311ed765368daacc601f9/profile/1698662184834-original.jpg',
-        blurHash: '',
-      },
-    },
-  ];
+  const { data: admins, isLoading } = useChannelAdmins(channelId);
+  const navigate = useNavigate();
   return (
-    <Card>
-      <div className="flex flex-col items-center justify-center p-8 ">
-        <img src={PrivateChannelImage} alt="private channel banner" />
-        <div className="text-2xl font-bold mt-8 mb-4">
+    <Card data-testid={dataTestId}>
+      <div
+        onClick={() => {
+          navigate(`channels/${channelId}`);
+        }}
+        className="flex flex-col items-center justify-center p-8 "
+      >
+        <img
+          src={PrivateChannelImage}
+          alt="private channel banner"
+          data-testid={`${dataTestId}-image`}
+        />
+        <div
+          className="text-2xl font-bold mt-8 mb-4"
+          data-testid={`${dataTestId}-header`}
+        >
           {t('privateChannel.bannerHeader')}
         </div>
         <Button
-          label={isChannelRequest ? 'Request to Join' : 'Withdraw request'}
+          label={
+            isChannelRequest
+              ? t('privateChannel.joinRequestCTA')
+              : t('privateChannel.withdrawRequestCTA')
+          }
           variant={isChannelRequest ? Variant.Primary : Variant.Danger}
           onClick={() => {}} // channel send or withdraw  function to call
+          data-testid={`${dataTestId}-button`}
         />
-        <div className="text-neutral-500 mt-[80px] underline  mb-4 text-sm font-semibold">
-          {t('privateChannel.spaceAdmins')}
+        <div
+          className="text-neutral-500 mt-[80px] underline  mb-4 text-sm font-semibold"
+          data-testid={`${dataTestId}-footer`}
+        >
+          {t('privateChannel.bannerFotter')}
         </div>
         <AvatarChips
-          users={adminDummyData} // admins users.
+          users={admins}
           showCount={7}
-          dataTestId="feed-post-shoutoutto-"
+          dataTestId={`${dataTestId}-avatar-chips`}
         />
       </div>
     </Card>
