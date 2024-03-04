93e1174c10130da6712654ea09ce33f0c36b30ea
Anshul Verma <anshul.verma@american-technology.net>
2024-03-04 23:50:09 +0530

    wireframe api in event widget.

--- src/queries/learn.ts
+++ src/queries/learn.ts
@@ -1,3 +1,4 @@
+import { QueryFunctionContext, useInfiniteQuery } from '@tanstack/react-query';
 import { ApiService, ProductEnum } from 'utils/apiService';
 import { getCookieValue, getCookieParam } from 'utils/misc';
 import { getItem } from 'utils/persist';
@@ -10,4 +11,31 @@ export const learnLogout = async () => {
   let url = `/users/logout?auth_token=${authToken}`;
   if (visitToken) url += `&visit_token=${visitToken}`;
   await learnAPIService.delete(url);
+};
+
+export const getAllEvents = async ({
+  pageParam = null,
+  queryKey,
+}: QueryFunctionContext<(Record<string, any> | undefined | string)[], any>) => {
+  if (pageParam === null) {
+    return await learnAPIService.get('/learner/events', queryKey[1]);
+  } else return await learnAPIService.get(pageParam);
+};
+export const useInfiniteLearnEvents = ({ q }: { q?: Record<string, any> }) => {
+  return useInfiniteQuery({
+    queryKey: ['learnEvent', q],
+    queryFn: getAllEvents,
+    getNextPageParam: (lastPage: any) => {
+      const pageDataLen = lastPage?.data?.result?.data?.length;
+      const pageLimit = lastPage?.data?.result?.paging?.limit;
+      if (pageDataLen < pageLimit) {
+        return null;
+      }
+      return lastPage?.data?.result?.paging?.next;
+    },
+    getPreviousPageParam: (currentPage: any) => {
+      return currentPage?.data?.result?.paging?.prev;
+    },
+    staleTime: 5 * 60 * 1000,
+  });
 };
