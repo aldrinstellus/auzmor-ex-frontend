f256d52ecc286e710979f560f57a748d5059c2dc
Shiv Agarwal <shiv@auzmor.com>
2024-03-11 09:12:01 +0530

    Make Events Empty State same for Learner and Admin

--- src/components/EventWidget/Component/EmptyState.tsx
+++ src/components/EventWidget/Component/EmptyState.tsx
@@ -1,33 +1,12 @@
-import Button from 'components/Button';
-import useRole from 'hooks/useRole';
-import { getLearnUrl } from 'utils/misc';
 import EventCalendar from 'images/eventCalendar.svg';
 
 const EmptyState = () => {
-  const { isAdmin } = useRole();
-  const header = isAdmin ? 'Create an event' : 'Upcoming events';
-  const footer = isAdmin
-    ? 'Only admins can see this'
-    : 'Upcoming events will appear here';
+  const header = 'Upcoming events';
+  const footer = 'Upcoming events will appear here';
   return (
     <div className="py-6 px-4 flex flex-col items-center gap-2">
       <div className="text-base font-bold line-clamp-2 ">{header}</div>
-      {isAdmin && (
-        <div className="text-xs text-neutral-500 font-normal">
-          When events are created, they appear on this card
-        </div>
-      )}
       <img src={EventCalendar} width={'132px'} height={'124px'} />
-      {isAdmin && (
-        <Button
-          label={'Create an event'}
-          className="w-full"
-          onClick={() => {
-            window.location.replace(`${getLearnUrl()}/events/create`);
-          }}
-        />
-      )}
-
       <div className="text-xs font-normal">{footer}</div>
     </div>
   );
