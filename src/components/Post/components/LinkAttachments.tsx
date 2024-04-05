3c2a7342efb7fbad39a63871859effa2a62b099c
Anshul Verma <anshul.verma@american-technology.net>
2024-04-05 13:37:32 +0530

    add document icon in LinkAttachments

--- src/components/Post/components/LinkAttachments.tsx
+++ src/components/Post/components/LinkAttachments.tsx
@@ -1,27 +1,45 @@
+import Icon from 'components/Icon';
+import moment from 'moment';
 import { LinkAttachment } from 'queries/post';
 import React, { FC } from 'react';
+import { ProductEnum } from 'utils/apiService';
+import { getItem } from 'utils/persist';
 
 interface ILinkAttachmentsProps {
   attachments: LinkAttachment[];
 }
 
 const LinkAttachments: FC<ILinkAttachmentsProps> = ({ attachments }) => {
+  const firstAttachment = attachments[0];
+
+  if (!firstAttachment) {
+    return null;
+  }
+
+  const { url, title } = firstAttachment;
+
+  const isImage = /(?:\.jpg|\.png)$/i.test(title);
+  const previewUrl = `${getItem(`${ProductEnum.Learn}RegionUrl`)}/attachments/${
+    url.split('/attachments/')[1].split('/')[0]
+  }/preview?auth_token=${getItem('uat')}&t=${moment()}`;
+
   return (
     <div className="flex gap-2">
-      {attachments.map((each) => (
-        <div
-          key={each?._id}
-          onClick={() => window.open(each.url)}
-          className="flex p-2 rounded-9xl border border-neutral-200 w-[173px] justify-center items-center gap-2 cursor-pointer hover:shadow-lg transition"
-        >
-          <div className="flex w-6 h-8">
-            <img className="object-cover" src={each.url} />
+      <div
+        key={firstAttachment._id}
+        onClick={() => window.open(previewUrl)}
+        className="flex  p-2 rounded-9xl border border-neutral-200 w-[173px] justify-center items-center gap-2 cursor-pointer hover:shadow-lg transition"
+      >
+        {isImage && (
+          <div className="flex w-6 h-6">
+            <img src={previewUrl} />
           </div>
-          <p className="text-xs font-medium max-w-[124px] truncate">
-            {each.title}
-          </p>
-        </div>
-      ))}
+        )}
+        <Icon name={title.substring(title.lastIndexOf('.') + 1)} />
+        <p className="text-xs font-medium max-w-[124px] truncate">
+          {title.substring(0, title.lastIndexOf('.'))}
+        </p>
+      </div>
     </div>
   );
 };
