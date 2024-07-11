6e4f966e7014c9134bc7a65c4c6204587fd35426
Anshul Verma <anshul.verma@american-technology.net>
2024-07-11 16:54:48 +0530

    LXP-344 add sort field in channel member tab

--- src/pages/ChannelDetail/components/Members/index.tsx
+++ src/pages/ChannelDetail/components/Members/index.tsx
@@ -60,6 +60,7 @@ const Members: React.FC<AppProps> = ({ channelData }) => {
     channelId: channelData?.id,
     q: isFiltersEmpty({
       q: searchValue,
+      sort: filters?.sort,
       userStatus: filters?.status?.length
         ? filters?.status?.map((eachStatus: any) => eachStatus.id).join(',')
         : undefined,
