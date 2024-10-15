c91013d67ae0d3bae5e9ee22de13884c4011105d
Shiv Agarwal <shiv@auzmor.com>
2024-10-15 13:56:47 +0530

    Remove hardcoded view type

--- src/components/NavbarLxp/index.tsx
+++ src/components/NavbarLxp/index.tsx
@@ -4,24 +4,23 @@ import ModernNavbar from './components/Navbar.modern';
 import ClassicNavbar from './components/Navbar.classic';
 import useRole from 'hooks/useRole';
 import AdminNavbar from './components/AdminNavbar.classic';
+import useAuth from 'hooks/useAuth';
+import { FRONTEND_VIEWS } from 'interfaces';
 
-interface INavbarLxpProps {
-  view: string;
-}
-
-const NavbarLxp: FC<INavbarLxpProps> = ({ view, ...rest }) => {
+const NavbarLxp: FC = () => {
   const { isOwnerOrAdmin } = useRole();
+  const { user } = useAuth();
 
   if (isOwnerOrAdmin) {
     return <AdminNavbar />;
   }
 
-  switch (view) {
-    case 'modern':
-      return <ModernNavbar {...rest} />;
+  switch (user?.preferences?.learnerViewType) {
+    case FRONTEND_VIEWS.modern:
+      return <ModernNavbar />;
 
     default:
-      return <ClassicNavbar {...rest} />;
+      return <ClassicNavbar />;
   }
 };
 
