6487aa85ee0a48151514626c3bb792ed2f12bb8d
DhruvinModi <dhruvin.m@american-technology.net>
2025-06-20 14:47:46 +0530

    Add support for Radio fields

--- src/components/FilterModalNew/components/GenericFilter.tsx
+++ src/components/FilterModalNew/components/GenericFilter.tsx
@@ -2,6 +2,7 @@ import { ICheckboxListOption } from 'components/CheckboxList';
 import Layout, { FieldType } from 'components/Form';
 import Icon from 'components/Icon';
 import NoDataFound from 'components/NoDataFound';
+import { IRadioListOption } from 'components/RadioGroup';
 import Truncate from 'components/Truncate';
 import { useDebounce } from 'hooks/useDebounce';
 import {
@@ -20,12 +21,13 @@ interface IGenericFilterProps<TFilters extends FieldValues> {
   setValue: UseFormSetValue<TFilters>;
   transform?: (
     options: Record<string, any>[] | string[] | number[],
-  ) => ICheckboxListOption[];
+  ) => ICheckboxListOption[] | IRadioListOption[];
+  listType?: 'CHECKBOX' | 'RADIO';
 }
 
 const transformOptions = (
   options: Record<string, any>[] | string[] | number[],
-): ICheckboxListOption[] => {
+): ICheckboxListOption[] | IRadioListOption[] => {
   if (!Array.isArray(options)) return [];
 
   if (typeof options[0] === 'string' || typeof options[0] === 'number') {
@@ -63,6 +65,7 @@ const GenericFilter = <TFilters extends Record<string, any>>({
   setValue,
   name,
   transform = transformOptions,
+  listType = 'CHECKBOX',
 }: IGenericFilterProps<TFilters>) => {
   const searchField = [
     {
@@ -76,7 +79,7 @@ const GenericFilter = <TFilters extends Record<string, any>>({
     },
   ];
 
-  const [search, optionCheckbox] = watch([
+  const [search, selectedOptions] = watch([
     `${name}-search`,
     name,
   ]) as Array<any>;
@@ -105,44 +108,68 @@ const GenericFilter = <TFilters extends Record<string, any>>({
       rowClassName: 'px-6 py-3 border-b border-neutral-200',
     },
   ];
+
+  const radioFields = [
+    {
+      type: FieldType.Radio,
+      name,
+      control,
+      radioList: optionList,
+      labelRenderer: (option: IRadioListOption) => (
+        <Truncate
+          text={option.data.label}
+          className="ml-2.5 cursor-pointer text-xs max-w-[200px]"
+        />
+      ),
+      rowClassName: 'px-6 py-3 border-b border-neutral-200',
+    },
+  ];
   return (
     <div className="px-2 py-4">
       <Layout fields={searchField} />
       <div className="max-h-[330px] min-h-[330px] overflow-y-auto">
-        {!!optionCheckbox?.length && (
+        {!!selectedOptions?.length && (
           <div className="flex mt-2 mb-3 flex-wrap">
-            {optionCheckbox.map((checkboxOption: ICheckboxListOption) => (
-              <div
-                key={checkboxOption.data.id}
-                data-testid="filter-options"
-                className="flex items-center px-3 py-2 bg-neutral-100 rounded-17xl border border-neutral-200 mr-2 my-1"
-              >
-                <Truncate
-                  text={checkboxOption.data.label}
-                  className="text-primary-500 text-sm font-medium whitespace-nowrap max-w-[128px]"
-                />
-                <div className="ml-1">
-                  <Icon
-                    name="closeCircle"
-                    size={16}
-                    color="text-neutral-900"
-                    onClick={() =>
-                      setValue(
-                        name as Path<TFilters>,
-                        optionCheckbox.filter(
-                          (selectedRoles: ICheckboxListOption) =>
-                            selectedRoles.data.id !== checkboxOption.data.id,
-                        ),
-                      )
-                    }
+            {selectedOptions.map(
+              (checkboxOption: ICheckboxListOption | IRadioListOption) => (
+                <div
+                  key={checkboxOption.data.id}
+                  data-testid="filter-options"
+                  className="flex items-center px-3 py-2 bg-neutral-100 rounded-17xl border border-neutral-200 mr-2 my-1"
+                >
+                  <Truncate
+                    text={checkboxOption.data.label}
+                    className="text-primary-500 text-sm font-medium whitespace-nowrap max-w-[128px]"
                   />
+                  <div className="ml-1">
+                    <Icon
+                      name="closeCircle"
+                      size={16}
+                      color="text-neutral-900"
+                      onClick={() =>
+                        setValue(
+                          name as Path<TFilters>,
+                          selectedOptions.filter(
+                            (
+                              selectedRoles:
+                                | ICheckboxListOption
+                                | IRadioListOption,
+                            ) =>
+                              selectedRoles.data.id !== checkboxOption.data.id,
+                          ),
+                        )
+                      }
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ),
+            )}
           </div>
         )}
         {options?.length > 0 ? (
-          <Layout fields={checkboxFields} />
+          <Layout
+            fields={listType === 'RADIO' ? radioFields : checkboxFields}
+          />
         ) : (
           <NoDataFound
             className="py-4 w-full h-full min-h-[330px] flex flex-col justify-center"
