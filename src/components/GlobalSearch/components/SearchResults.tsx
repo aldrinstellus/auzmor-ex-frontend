9174a1ca049ba88f9b766933e704ce71a7325f68
syedali11 <syedali@atc.xyz>
2025-08-04 13:48:25 +0530

    Added the support for the custom fields hyperlink

--- src/components/GlobalSearch/components/SearchResults.tsx
+++ src/components/GlobalSearch/components/SearchResults.tsx
@@ -413,7 +413,7 @@ const SearchResults: FC<ISearchResultsProps> = ({
                 <HighlightText
                   text={Array.isArray(matched.fieldValues)
                     ? matched.fieldValues.find((val: any) => val?.toLowerCase?.().includes(searchQuery?.toLowerCase?.()))
-                    : matched.fieldValues}
+                    : typeof matched.fieldValues === 'object' ? matched.fieldValues.Description : matched.fieldValues}
                   subString={searchQuery}
                 />
                 &quot;&nbsp;
